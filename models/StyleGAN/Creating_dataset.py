dc46af882ceddca5d77e8f797a114c367cf6bd2e
akn0717 <magicmonster17@gmail.com>
2021-05-19 09:51:30 +0700

    Update new feature: creating hdf5 dataset

---
+++ models/StyleGAN/Creating_dataset.py
@@ -0,0 +1,33 @@
+import h5py
+
+import numpy as np
+import argparse
+import os
+from utils.imglib import *
+
+def customize(args):
+    custom_data_dir = args.source
+
+    H = W = 64
+    if (args.size == 1):
+        H = W = 256
+
+    img_files = [f for f in os.listdir(custom_data_dir) if os.path.isfile(os.path.join(custom_data_dir, f))]
+
+    with h5py.File(args.dest, mode = 'w') as f:
+        for file in img_files:
+            img = load_image(os.path.join(custom_data_dir,file),H,W)
+            f.create_dataset(file, data = img, chunks = True)
+
+    return 0
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--source', dest = 'source', default = 'custom_dataset')
+    parser.add_argument('-r', '--resolution', dest = 'size', default = 0)
+    parser.add_argument('-d', '--destination', dest = 'dest', default = 'dataset.h5')
+    args = parser.parse_args()
+    
+    customize(args)
--- models/StyleGAN/utils/imglib.py
+++ models/StyleGAN/utils/imglib.py
@@ -2,6 +2,11 @@ from keras.preprocessing import image
 import os
 import numpy as np
 
+def load_image(data_dir, W, H):
+    img_load = image.load_img(data_dir,target_size = (W,H))
+    
+    return np.reshape(img_load,(W,H,3))
+
 def load_images(data_dir, W, H):
     img_list = []
     img_files = [f for f in os.listdir(data_dir) if os.path.isfile(os.path.join(data_dir, f))]
